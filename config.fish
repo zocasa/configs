b5b4fab9cba9437dd51fa73994bf05914ca986c3
zocasa <122727555+zocasa@users.noreply.github.com>
2023-01-16 02:22:34 -0800

    Updated for fish Ubuntu

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g fish_greeting
+
 # To set nice prompt messages
 function fish_prompt
     set_color cyan
@@ -11,9 +13,9 @@ end
 set -g fish_prompt_pwd_dir_length 0
 
 function fish_right_prompt
-    set -g __fish_git_prompt_show_informative_status
-    set -g __fish_git_prompt_showstashstate
-    set -g __fish_git_prompt_showcolorhints
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_showstashstate 1
+    set -g __fish_git_prompt_showcolorhints 1
     set -g __fish_git_prompt_color_branch --bold purple
     set -g __fish_git_prompt_color_upstream normal
     set -g __fish_git_prompt_color_cleanstate green
@@ -23,5 +25,3 @@ function fish_right_prompt
     echo (set_color yellow)(prompt_pwd)
     echo (set_color normal)(fish_git_prompt)
 end
-
-set -g fish_greeting
